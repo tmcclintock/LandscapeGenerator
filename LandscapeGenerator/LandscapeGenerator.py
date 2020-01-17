048c3cbe59ff03535e3432b5d0eac2eb4f0b5aba
Tom McClintock <tmcclintock89@gmail.com>
2020-01-17 09:17:34 -0500

    Added asserts.

--- LandscapeGenerator/LandscapeGenerator.py
+++ LandscapeGenerator/LandscapeGenerator.py
@@ -2,6 +2,8 @@ import numpy as np
 
 class Landscape(object):
     def __init__(self, dimensions, kind="grassy-field"):
+        assert len(dimensions) == 2
+        assert len(np.shape(dimensions)) == 1
         self.dimensions = dimensions
         self.kind = kind
 
