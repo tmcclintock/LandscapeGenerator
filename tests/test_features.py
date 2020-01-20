16e9102f9af6657449139eec073cb76b2143d72b
Tom McClintock <tmcclintock89@gmail.com>
2020-01-20 12:48:42 -0500

    Made tests for the features.

---
+++ tests/test_features.py
@@ -0,0 +1,52 @@
+import LandscapeGenerator as LG
+import LandscapeGenerator.Features as LGF
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+def test_Feature_creation():
+    #Smoke tests
+    LGF.SkyFeature()
+    LGF.GrassFeature()
+    LGF.SunFeature()
+    LGF.SkyGradientFeature()
+
+def test_SkyFeature():
+    M, N = 300, 200
+    L = LG.LGenerator([M, N])
+    L.add_feature(LGF.SkyFeature())
+    rgb = L.generate()
+    npt.assert_equal(len(rgb), M)
+    npt.assert_equal(len(rgb[0]), N)
+    npt.assert_equal(len(rgb[0][0]), 3)
+    npt.assert_equal(rgb.shape, (M, N, 3))
+
+def test_GrassFeature():
+    M, N = 100, 50
+    L = LG.LGenerator([M, N])
+    L.add_feature(LGF.GrassFeature())
+    rgb = L.generate()
+    npt.assert_equal(len(rgb), M)
+    npt.assert_equal(len(rgb[0]), N)
+    npt.assert_equal(len(rgb[0][0]), 3)
+    npt.assert_equal(rgb.shape, (M, N, 3))
+
+def test_SunFeature():
+    M, N = 150, 250
+    L = LG.LGenerator([M, N])
+    L.add_feature(LGF.SunFeature())
+    rgb = L.generate()
+    npt.assert_equal(len(rgb), M)
+    npt.assert_equal(len(rgb[0]), N)
+    npt.assert_equal(len(rgb[0][0]), 3)
+    npt.assert_equal(rgb.shape, (M, N, 3))
+
+def test_SkyGradientFeature():
+    M, N = 120, 120
+    L = LG.LGenerator([M, N])
+    L.add_feature(LGF.SkyGradientFeature())
+    rgb = L.generate()
+    npt.assert_equal(len(rgb), M)
+    npt.assert_equal(len(rgb[0]), N)
+    npt.assert_equal(len(rgb[0][0]), 3)
+    npt.assert_equal(rgb.shape, (M, N, 3))
